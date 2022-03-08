2c26e82a0de41094000ec630c1419ea477856403
Byron Lathi <bslathi19@gmail.com>
2022-03-08 15:41:03 -0600

    Remove io c code

--- sw/io.c
+++
@@ -1,6 +0,0 @@
-#include <io.h>
-
-int io_remap(int region) {
-    *(unsigned char*)IO_REMAP = region;
-    return (*(unsigned char*)IO_REMAP == region);
-}
--- sw/main.c
+++ sw/main.c
@@ -1,6 +1,5 @@
 #include <io.h>
 
 int main() {
-    io_remap(io_region_de10lite);
     return 0;
 }
