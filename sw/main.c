a671fda51c8a74d7e3657d6ba6808aee3fd58725
Byron Lathi <bslathi19@gmail.com>
2022-03-14 10:46:36 -0500

    Add sample puts function
    "Hello, world!" :)

--- sw/main.c
+++ sw/main.c
@@ -1,10 +1,24 @@
 #include <stdint.h>
 
 #include "sevenseg.h"
+#include "uart.h"
+
+void puts(const char* s)
+{
+    while (*s) {
+        uart_txb_block(*s);
+        if (*s == '\n')
+            uart_txb_block('\r');
+
+        s++;
+    }
+}
 
 int main() {
     hex_enable(0x3f);
     hex_set_24(0xabcdef);
+
+    puts("Hello, World!\n");
     while(1);
     return 0;
 }
