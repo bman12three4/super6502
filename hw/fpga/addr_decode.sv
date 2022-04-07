a15dde0e89f8a97050d073db0e331e6c171808e2
Byron Lathi <bslathi19@gmail.com>
2022-04-07 10:32:28 -0500

    Add memory mapper software interface
    Adds functions to read and write mappings, as well as enable and disable
    the memory mapper.
    
    This also moves increases the io space by 16 bytes.

--- hw/fpga/addr_decode.sv
+++ hw/fpga/addr_decode.sv
@@ -11,12 +11,12 @@ module addr_decode(
 );
 
 assign rom_cs = addr >= 16'h8000;
-assign sdram_cs = addr < 16'h7ff0;
+assign sdram_cs = addr < 16'h7fe0;
+assign mm_cs1 = addr >= 16'h7fe0 && addr < 16'h7ff0;
 assign hex_cs = addr >= 16'h7ff0 && addr < 16'h7ff4;
 assign uart_cs = addr >= 16'h7ff4 && addr < 16'h7ff6;
 assign board_io_cs = addr == 16'h7ff6;
 assign mm_cs2 = addr == 16'h7ff7;
-assign mm_cs1 = addr >= 16'h7ff8 && addr < 16'h7ffc;
 assign irq_cs  = addr == 16'h7fff;
 
 endmodule
