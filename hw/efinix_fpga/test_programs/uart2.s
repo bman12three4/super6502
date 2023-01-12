519cd19739dd89f53437cfee15483d2e46e2a27e
Byron Lathi <bslathi19@gmail.com>
2023-01-12 14:05:13 -0600

    Add uart2 test program
    Reads input from uart and then writes it back out again immediately.

--- hw/efinix_fpga/test_programs/uart2.s
+++ hw/efinix_fpga/test_programs/uart2.s
@@ -8,24 +8,12 @@ UART_CONTROL = UART_STATUS
 main:
     ldx #$00
 loop:
-    lda string,x
-    beq end
-    sta UART_TX
-    inx
-wait:
-    lda UART_STATUS
-    bit #$02
+    lda UART_STATUS ; see if bit 0 is set
+    bit #$01
     beq loop
-    bra wait
-
-end:
-    wai
-    bra end
-
-
-
-string:
-    .asciiz "Hello, world!"
+    lda UART_RX     ; read rx buffer if so
+    sta UART_TX     ; transmit it back again
+    bra loop
 
 
 .segment "VECTORS"
