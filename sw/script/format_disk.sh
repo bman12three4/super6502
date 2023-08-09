d191c1da5bc8c2361002fff4c04b31ea9c207e54
Byron Lathi <bslathi19@gmail.com>
2023-08-08 23:52:57 -0700

    Modify disk script to leave partition table intact

--- sw/script/format_disk.sh
+++ sw/script/format_disk.sh
@@ -13,7 +13,7 @@ sudo mkfs.vfat -F32 $DEVICE -n SUPER6502 $V
 echo
 
 echo "$(tput bold setaf 11)Modifying Boot Sector$(tput sgr 0)"
-sudo dd if=$BOOTLOADER of=$DEVICE bs=512 count=1 $STATUS
+sudo dd if=$BOOTLOADER of=$DEVICE bs=446 count=1 $STATUS
 
 echo "$(tput bold setaf 10)Done!$(tput sgr 0)"
 
