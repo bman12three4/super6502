4b415ed2ba68f94c125d6f55c385119b5b0f5b06
Byron Lathi <bslathi19@gmail.com>
2023-10-20 17:20:11 -0700

    Update C test to actually use mapper a little bit

--- sw/test_code/mapper_test/main.s
+++ sw/test_code/mapper_test/main.s
@@ -14,10 +14,51 @@ _init:
     lda #$10
     sta MAPPER_BASE + 2
 
+    ; This should store 0x55aa to memory $010000, instead of $001000
+
     lda #$aa
     sta $1000
     lda #$55
     sta $1001
 
+    lda #$01
+    sta MAPPER_BASE + 2
 
-@end:   bra @end
+    ; This should store 0xddcc to memory $001000
+
+    lda #$cc
+    sta $1000
+    lda #$dd
+    sta $1001
+
+    lda #$10
+    sta MAPPER_BASE + 2
+
+    lda $1000
+    cmp #$aa
+    bne @bad
+    lda $1001
+    cmp #$55
+    bne @bad
+
+    lda #$01
+    sta MAPPER_BASE + 2
+
+    lda $1000
+    cmp #$cc
+    bne @bad
+    lda $1001
+    cmp #$dd
+    bne @bad
+
+@end:   
+    lda #$6d
+    sta $00
+    bra @end
+
+
+@bad:
+    lda #$bd
+    sta $00
+    bra @bad
+
