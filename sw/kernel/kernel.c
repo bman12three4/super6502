e3662e703c18f0963b362a9ba3b1234ec524336b
Byron Lathi <bslathi19@gmail.com>
2023-10-30 00:04:57 -0700

    Add back long string

--- sw/kernel/kernel.c
+++ sw/kernel/kernel.c
@@ -1,14 +1,14 @@
 #include <conio.h>
 
-// char* longstring = \
-// "This is a very long string that is meant to test the loader.\
-//  We can only load one cluster so far, which means 8 sectors of\
-//  512bytes, or a total of 4k. If there was any more data than this,\
-//  then we would have to traverse the fat to find the next cluster number.\
-//  This may not be that difficult, but the file will need to be large\
-//  enough to actually stretch that far. The kernel will probably be\
-//  that big in the future, but for now when it doesnt really do anything\
-//  then it can't really be tested.";
+char* longstring = \
+"This is a very long string that is meant to test the loader.\
+ We can only load one cluster so far, which means 8 sectors of\
+ 512bytes, or a total of 4k. If there was any more data than this,\
+ then we would have to traverse the fat to find the next cluster number.\
+ This may not be that difficult, but the file will need to be large\
+ enough to actually stretch that far. The kernel will probably be\
+ that big in the future, but for now when it doesnt really do anything\
+ then it can't really be tested.";
 
 
 int main() {
@@ -19,7 +19,7 @@ int main() {
     cprintf("Val: %d\n", val);
     cprintf("%s", string);
 
-    // cprintf("Here is a long string: %s\n", longstring);
+    cprintf("Here is a long string: %s\n", longstring);
 
     while(1);
 
