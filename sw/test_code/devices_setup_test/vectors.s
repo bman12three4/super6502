0232d9c89f71c4b6c342e51f2d38d51a12d5ca8b
Byron Lathi <bslathi19@gmail.com>
2023-11-21 20:20:25 -0800

    Rename symbols again

--- sw/test_code/devices_setup_test/vectors.s
+++ sw/test_code/devices_setup_test/vectors.s
@@ -5,10 +5,10 @@
 ; Defines the interrupt vector table.
 
 .import    _init
-.import    _nmi_int, _irq_int
+.import    nmi_int, irq_int
 
 .segment  "VECTORS"
 
-.addr      _nmi_int    ; NMI vector
+.addr      nmi_int    ; NMI vector
 .addr      _init       ; Reset vector
-.addr      _irq_int    ; IRQ/BRK vector
+.addr      irq_int    ; IRQ/BRK vector
