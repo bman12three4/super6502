991cd24e732e3e9a4f7a5440dce38e0613019897
Byron Lathi <bslathi19@gmail.com>
2024-07-15 22:39:27 -0700

    Change to using modules

--- init_env.sh
+++ init_env.sh
@@ -6,16 +6,12 @@
 export REPO_TOP=$(git rev-parse --show-toplevel)
 export KICAD7_SYMBOL_DIR=$REPO_TOP/hw/kicad_library/symbols
 export KICAD7_3DMODEL_DIR=$REPO_TOP/hw/kicad_library/3dmodels
-export KICAD7_FOOTPRINT_DIR=$REPO_TOP/hw/kicad_library/footprints	
+export KICAD7_FOOTPRINT_DIR=$REPO_TOP/hw/kicad_library/footprints
 
 
 python3.11 -m venv .user_venv
 . .user_venv/bin/activate
 
-if [ -n "$EFX_SETUP" ]; then
-    source $EFX_SETUP
-else
-    echo "EFX_SETUP not defined!"
-fi
+module load efinity/2023.1
 
 # pip install -r requirements.txt
