247033ea2d71c5a230780e88f1e414122e43dd87
Byron Lathi <bslathi19@gmail.com>
2024-09-01 22:26:39 -0700

    Uncomment module load
    oops

--- init_env.sh
+++ init_env.sh
@@ -14,6 +14,6 @@ python3.11 -m venv .user_venv
 
 pip install -r requirements.txt
 
-# module load efinity/2023.1
-# module load verilator
-# module load gtkwave/3.3_gtk3
+module load efinity/2023.1
+module load verilator
+module load gtkwave/3.3_gtk3
