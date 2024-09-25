d9e33b65ce29ba64804dc10132e5a53ab9083b8d
Byron Lathi <bslathi19@gmail.com>
2024-09-24 22:29:44 -0700

    Update

--- init_env.sh
+++ init_env.sh
@@ -9,10 +9,10 @@ export KICAD8_3DMODEL_DIR=$REPO_TOP/hw/kicad_library/3dmodels
 export KICAD8_FOOTPRINT_DIR=$REPO_TOP/hw/kicad_library/footprints
 
 
-python3.11 -m venv .user_venv
+python3.12 -m venv .user_venv
 . .user_venv/bin/activate
 
-module load efinity/2023.1
+#module load efinity/2023.1
 module load iverilog/12.0
 module load gtkwave/3.3_gtk3
 
